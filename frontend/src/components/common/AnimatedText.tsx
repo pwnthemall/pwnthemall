c1626247b738d8f57cfc66536ba381b7b750f9e1
swinowz <97311975+swinowz@users.noreply.github.com>
2026-01-26 11:22:31 +0100

    fix(build): fixed pta not building in prod

--- frontend/src/components/common/AnimatedText.tsx
+++ frontend/src/components/common/AnimatedText.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from '../styles/AnimatedText.module.css';
+import styles from '../../styles/AnimatedText.module.css';
 
 interface AnimatedTextProps {
   text: string;
